be986d43ca5145e3aab9ed76957757118cb160fb
123 <123@gmail.com>
2023-05-18 15:18:52 +0530

    'app.js'

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req,res)=> {
+	res.send('<h1> Welcome to new app</h1>');
+})
+
+app.listen(3000,()=>{
+	console.log('demo app is listening to : ${port}');
+})
