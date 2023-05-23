1aa9413019f52f523d3b2ef0e1f9ec7251595be4
123 <123@gmail.com>
2023-05-23 16:05:33 +0530

    'app'

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -15,11 +15,11 @@ cd /home/ec2-user
 
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-
+chmod +x ~/.nvm/nvm.sh
 export NVM_DIR="$HOME/.nvm/"
 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR\nvm.sh" ] && \. "$NVM_DIR\nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR\bash_completion" ] && \. "$NVM_DIR\bash_completion"  # This loads nvm bash_completion
 
 nvm install 14
 
