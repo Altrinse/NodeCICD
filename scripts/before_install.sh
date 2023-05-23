76818d91797dc288d219cd6ae7ae2e65f626bc75
123 <123@gmail.com>
2023-05-23 11:33:00 +0530

    'app'

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -15,7 +15,7 @@ cd /home/ec2-user/NodeCICD
 
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
+./nvm/nvm.sh
 nvm install 14
 
 
